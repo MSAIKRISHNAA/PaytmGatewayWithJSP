2d3fabc912e17539c135084c903112bf9283cead
Krishna <Krishna@192.168.10.7>
2019-04-06 16:21:38 +0530

    added mechant key and MID into paytmconstants.java

--- mywebsite/src/main/java/paytm_java/PaytmConstants.java
+++ mywebsite/src/main/java/paytm_java/PaytmConstants.java
@@ -1,7 +1,8 @@
 package paytm_java;
 public class PaytmConstants {
-  public final static String MID=""; //parameters obtained after paytm merchant signup in paytm website
-  public final static String MERCHANT_KEY=""; //parameters obtained after paytm merchant signup in paytm website
+	
+	public final static String MID="KBEMtH72671885950333"; //parameters obtained after paytm merchant signup in paytm website
+	public final static String MERCHANT_KEY="j%dTTGV%9eaDHf_l";//parameters obtained after paytm merchant signup in paytm website
   public final static String INDUSTRY_TYPE_ID="Retail"; //parameters obtained after paytm merchant signup in paytm website
   public final static String CHANNEL_ID="WEB"; //parameters obtained after paytm merchant signup in paytm website
   public final static String WEBSITE="WEBSTAGING";  //parameters obtained after paytm merchant signup in paytm website
